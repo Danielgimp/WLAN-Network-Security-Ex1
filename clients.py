b99f91a682e52cdf272e8bcb26be191e0d967816
Danielgimp <57535036+Danielgimp@users.noreply.github.com>
2021-03-17 10:42:34 +0200

    Add files via upload

--- clients.py
+++ clients.py
@@ -15,15 +15,15 @@ def pkt_callback(pkt):
         if bss not in APs:
             APs.append(bss)
 
-    elif pkt.haslayer(Dot11) and pkt.getlayer(Dot11).type == 2L and not pkt.haslayer(EAPOL):
+    elif pkt.haslayer(Dot11) and pkt.getlayer(Dot11).type == 2 and not pkt.haslayer(EAPOL):
         # This means it's data frame.
         sn = pkt.getlayer(Dot11).addr2.upper()
         rc = pkt.getlayer(Dot11).addr1.upper()
 
         if sn in APs:
-            print "AP (%s) [%s] > STA (%s) [%s]" % (sn, manf(sn), rc, manf(rc))
+            print ("AP (%s) [%s] > STA (%s) [%s]" % (sn, manf(sn), rc, manf(rc)))
         elif rc in APs:
-            print "AP (%s) [%s] < STA (%s) [%s]" % (rc, manf(rc), sn, manf(sn))
+            print ("AP (%s) [%s] < STA (%s) [%s]" % (rc, manf(rc), sn, manf(sn)))
 
 if __name__ == "__main__":
     sniff(iface="wlxc83a35c2e0b8", prn=pkt_callback)
